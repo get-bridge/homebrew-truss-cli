7ca3154298e44bc83bbf929e4de517912b76c9ca
Adam Engebretson <adam.engebretson@instructure.com>
2020-06-05 18:24:40 -0500

    Updating to 0.0.6

--- Formula/truss-cli.rb
+++ Formula/truss-cli.rb
@@ -1,7 +1,7 @@
 class TrussCli < Formula
   desc "CLI to help manage many k8s clusters"
   homepage "https://github.com/instructure-bridge/truss-cli"
-  version "0.0.5"
+  version "0.0.6"
 
   depends_on "kubectl"
   depends_on "vault"
@@ -21,10 +21,10 @@ class TrussCli < Formula
 
   url "https://github.com/instructure-bridge/truss-cli/releases/download/v#{version}/#{basename}"
   sha256({
-    "truss-cli_0.0.5_Linux_x86_64.tar.gz" => "1e74aec6c423a171afb6b47d89db74953cd5f6312621474cf7097151b0c6fbce",
-    "truss-cli_0.0.5_Linux_i386.tar.gz" => "4eab713b7d2df3436e472eb8c7193c9da6c7cb772544c0594ca507a69be7a38c",
-    "truss-cli_0.0.5_Darwin_i386.tar.gz" => "8ae8c44ae7fc7b0bc6dfe2df134965d41c4d94ede7763c6b7a599830a095a0ec",
-    "truss-cli_0.0.5_Darwin_x86_64.tar.gz" => "c757d67522634c84f12d6a6faadd742679d7c56dfb39e6d8fa16a666e2544f2f",
+    "truss-cli_0.0.6_Linux_x86_64.tar.gz" => "2ad20af8b9ee40f961ea08065b510ac1e004842a15eb06d680e84de652c0dcfc",
+    "truss-cli_0.0.6_Linux_i386.tar.gz" => "1c8018856ea52823ab1f946a1e0add058269b01faac6d9273110a716ca81ce78",
+    "truss-cli_0.0.6_Darwin_i386.tar.gz" => "b6706449b9dc8773597fb720cff412ce163d58365e55db5cd61cff940a8b1621",
+    "truss-cli_0.0.6_Darwin_x86_64.tar.gz" => "8dcf848fa3a18f585b54a9e870023d4decf20095d96b7d0983b14adf65ee6ef1",
   }[basename])
 
   def install
